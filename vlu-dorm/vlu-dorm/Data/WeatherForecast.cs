6b7624221c4490018dd5b232b00455325176776d
Mộc Dương Lâm <mocduonglam86@gmail.com>
2022-02-20 17:09:29 +0700

    start

--- vlu-dorm/vlu-dorm/Data/WeatherForecast.cs
+++ vlu-dorm/vlu-dorm/Data/WeatherForecast.cs
@@ -1,3 +1,5 @@
+using System;
+
 namespace vlu_dorm.Data
 {
     public class WeatherForecast
@@ -8,6 +10,6 @@ namespace vlu_dorm.Data
 
         public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
 
-        public string? Summary { get; set; }
+        public string Summary { get; set; }
     }
 }
